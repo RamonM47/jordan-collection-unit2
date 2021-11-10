e9a09e6037247bebefc237da1e07df8fde68b02f
RamonM47 <aviramon.mesa@gmail.com>
2021-11-09 19:51:13 -0500

    deleted all files and started with a new Oauth template. going for full CRUD without an API

--- models/profile.js
+++ models/profile.js
@@ -1,26 +1,8 @@
 import mongoose from 'mongoose'
-const Schema = mongoose.Schema
-
-
-const reviewsSchema = new Schema({
-  review: String,
-  author: {type: Schema.Types.ObjectId, ref: 'Profile'}
-}, {
-  timestamps: true
-})
-
-const jordanSchema = new Schema ({
-  name: String,
-  shoe_condition: {type: String, enum: ['new_with_defects', 'new_no_defects', 'used']},
-  reviews: [reviewsSchema]
-})
-
-
 
 const profileSchema = new mongoose.Schema({
   name: String,
   avatar: String,
-  jordans: [jordanSchema]
 }, {
   timestamps: true
 })
--- routes/index.js
+++ routes/index.js
@@ -6,13 +6,6 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
 })
 
-function isLoggedIn (req,res,next) {
-  if (req.isAuthenticated()) {
-      return done()
-  }
-  res.redirect('/auth/google')
-}
-
 export {
   router
 }
