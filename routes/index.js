3d5c9ae6a7fdc7acbd51154cb39ececbbab5e976
RamonM47 <aviramon.mesa@gmail.com>
2021-11-12 00:12:45 -0500

    styling done complete CRUD Full google auth.. Project done!

--- routes/index.js
+++ routes/index.js
@@ -6,7 +6,10 @@ router.get('/', function (req, res) {
   res.render('index', { title: 'Home Page', user: req.user ? req.user : null })
 })
 
-
+function isLoggedIn(req, res, next) {
+  if (req.isAuthenticated()) return next();
+  res.redirect("/auth/google");
+}
 
 export {
   router
--- routes/jordans.js
+++ routes/jordans.js
@@ -2,13 +2,18 @@ import { Router } from "express"
 const router = Router()
 import * as jordansCtrl from '../controllers/jordans.js'
 
-router.get('/', jordansCtrl.index)
-router.get('/new', jordansCtrl.new)
-router.get('/:id', jordansCtrl.show)
-router.get('/:id/edit', jordansCtrl.edit)
-router.put('/:id', jordansCtrl.update)
-router.post('/', jordansCtrl.create)
-router.delete('/:id', jordansCtrl.delete)
+router.get('/', isLoggedIn, jordansCtrl.index)
+router.get('/new', isLoggedIn, jordansCtrl.new)
+router.get('/:id', isLoggedIn, jordansCtrl.show)
+router.get('/:id/edit', isLoggedIn, jordansCtrl.edit)
+router.put('/:id', isLoggedIn, jordansCtrl.update)
+router.post('/', isLoggedIn, jordansCtrl.create)
+router.delete('/:id', isLoggedIn, jordansCtrl.delete)
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) return next();
+    res.redirect("/auth/google");
+}
 
 export {
     router
